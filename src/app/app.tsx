1b231e734dc80c33e38611473b6b46f1f8179c00
Jeremy <none>
2018-03-04 14:48:55 -0500

    init

---
+++ src/app/app.tsx
@@ -0,0 +1,43 @@
+import 'tslib'
+import 'reflect-metadata'
+import { remote } from 'electron';
+import * as React from 'react';
+import { render } from 'react-dom';
+
+import Button from './components/Button';
+
+const main = remote.require("./main").FuseBox.main("default/desktop/main.js");
+
+interface Props {
+}
+interface State {
+  body: string;
+}
+
+class Main extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      body: "hello world",
+    };
+  }
+
+  render(): JSX.Element {
+    const {body} = this.state;
+
+    return (
+      <div>
+        <h1>sample app</h1>
+        <p>{body}</p>
+        <Button onClick={this.handleClick}>styled button</Button>
+      </div>
+    );
+  }
+
+  handleClick() {
+    alert(main.fn());
+  }
+}
+
+render(<Main />, document.getElementById("root"));
---
+++ src/desktop/installExtensions.ts
@@ -0,0 +1,17 @@
+const installer = require('electron-devtools-installer');
+
+//require('electron-debug')({ showDevTools: true });
+
+
+export const installExtensions = async () => {
+        const extensions = ['REACT_DEVELOPER_TOOLS', 'REACT_PERF', 'REDUX_DEVTOOLS'];
+        for (const extension of extensions)
+          {
+            try {installer.default(installer[extension])} 
+            
+            catch (error) {console.log(error)}
+          }
+
+    }
+
+  
